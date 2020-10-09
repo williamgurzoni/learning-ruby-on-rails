1f3083efd33b1c2cc6adad6d0702541791ac6c8a
William Gurzoni <william_luizat@hotmail.com>
2020-10-09 21:47:55 +1300

    Adding Comment model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
-
   def index
     @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def edit
     @article = Article.find(params[:id])
   end
 
@@ -22,6 +26,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  
+    redirect_to articles_path
   end
    
   private
